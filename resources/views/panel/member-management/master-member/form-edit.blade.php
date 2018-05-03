c38c0ca4a5f6d4d33e24b207e6f54468223801e6
andri <andri@fiture.id>
2018-05-04 06:20:46 +0700

    update client

--- resources/views/panel/member-management/master-member/form-edit.blade.php
+++ resources/views/panel/member-management/master-member/form-edit.blade.php
@@ -147,9 +147,9 @@
 				<div class="card">
 					<p>
 					<div class="btn-group"><span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-			          	<button type="submit" class="btn btn-success">Save</button>&nbsp;
-			        <button type="button" class="btn btn-secondary" onclick="window.history.back()">
-			          <i class="fa fa-times-rectangle"></i>&nbsp; Cancel
+			          	<button type="submit" name="save" id="save" class="btn btn-success">Save</button>&nbsp;
+			        	<button type="button" class="btn btn-secondary" onclick="window.history.back()">
+			          	<i class="fa fa-times-rectangle"></i>&nbsp; Cancel
 			        </button>
 			    	</div>
 					</p>
@@ -180,6 +180,19 @@
 			reader.readAsDataURL(input.files[0]);
 		}
 	}
+
+	$(document).on('click', '#save', function(e) {
+        e.preventDefault();
+        if($('#jxForm1').valid()){
+            swal({
+                title: "Are you sure want to submit the form?",
+                text: "Please make sure all data inputted correctly",
+                buttons: true,
+            }).then((confirm) => {
+                if(confirm){ $('#jxForm1').submit(); }
+            });
+        }
+    });
 
 	$("#picture").change(function (){ readURL(this); });
 
