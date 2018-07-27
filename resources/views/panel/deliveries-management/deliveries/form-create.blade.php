4352dbaff9ce57aa4cd793549871e5b457216a6e
andri <andri@fiture.id>
2018-07-27 16:07:32 +0700

    update view delivery

--- resources/views/panel/deliveries-management/deliveries/form-create.blade.php
+++ resources/views/panel/deliveries-management/deliveries/form-create.blade.php
@@ -14,11 +14,11 @@
             <div class="col-md-7">
               <div class="card">
                 <div class="card-header">
-                  <i class="fa fa-ticket"></i> Deliveries PO Status
+                  <i class="fa fa-ticket"></i> Transaction Status
                 </div>
                 <div class="card-body">
                   <table class="table table-responsive-sm">
-                    <thead>
+                    <thead> <h5>Status history</h5></p>
                       <tr>
                         <th>Status</th>
                         <th>Date</th>
@@ -26,8 +26,8 @@
                     </thead>
                     <tbody>
                       <tr>
-                        <td>Processed</td>
-                        <td>2018/02/01</td>
+                        <td>{{$orders->status}}</td>
+                        <td>{{$orders->created_at}}</td>
                       </tr>
                     </tbody>
                   </table>
@@ -35,7 +35,53 @@
               </div>
               <div class="card">
                 <div class="card-header">
-                  <i class="fa fa-id-card"></i> Data Member
+                  <i class="fa fa-ticket"></i> Data Sales
+                </div>
+                <div class="card-body">
+                  <table class="table table-responsive-sm">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{{(isset($orders->sales[0]['name'])?$orders->sales[0]['name']:'')}}</td>
+                        <td>{{(isset($orders->sales[0]['detail']['email'])?$orders->sales[0]['detail']['email']:'')}}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header">
+                  <i class="fa fa-ticket"></i> Data Discount
+                </div>
+                <div class="card-body">
+                  <table class="table table-responsive-sm">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Value</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{{(isset($orders->discount[0]['name'])?$orders->discount[0]['name']:'')}}</td>
+                        <td>{{(isset($orders->discount[0]['value'])?$orders->discount[0]['value']:'')}}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <!--/.col-->
+
+            <div class="col-md-5">
+              <div class="card">
+                <div class="card-header">
+                  <i class="fa fa-user"></i> Data Member
                 </div>
                 <div class="card-body">
                   <table class="table table-responsive-sm">
@@ -47,39 +93,14 @@
                     </thead>
                     <tbody>
                       <tr>
-                        <td>Member</td>
-                        <td>member@ecommerce.com</td>
+                        <td>{{(isset($orders->client[0]['display_name'])?$orders->client[0]['display_name']:'')}}</td>
+                        <td>{{(isset($orders->client[0]['email'])?$orders->client[0]['email']:'')}}</td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
               </div>
-              <!-- <div class="card">
-                <div class="card-header">
-                  <span><i class="fa fa-credit-card"></i> Payment Method</span>
-                </div>
-                <div class="card-body">
-                  <table class="table table-condensed">
-                    <thead>
-                      <tr>
-                        <th>Type Payment</th>
-                        <th>Name Bank</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td class="vertical-align-middle">ATM Transfer</td>
-                        <td class="vertical-align-middle">MANDIRI</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div> -->
-            </div>
-            <!--/.col-->
-
-            <div class="col-md-5">
-               <div class="card">
+              <div class="card">
                 <div class="card-header with-border">
                       <span><i class="fa fa-user"></i> Shipping details</span>
                 </div>
@@ -89,123 +110,17 @@
                         <li class="nav-item">
                           <a class="nav-link active show" data-toggle="tab" href="#tab-shipping-address" role="tab" aria-controls="home" aria-selected="true">Shipping address</a>
                         </li>
-                        <li class="nav-item">
-                          <a class="nav-link" data-toggle="tab" href="#tab-billing-info" role="tab" aria-controls="home" aria-selected="true">Billing info</a>
-                        </li>
                     </ul>
                     <div class="tab-content">
                       <div class="tab-pane active" id="tab-shipping-address">
-                        <h5>Shipping address</h5>
                           <table class="table table-condensed table-hover">
-                            <tbody><tr>
-                              <td>Contact Person</td>
-                              <td>Jerry Williams</td>
-                            </tr>
+                            <tbody>
                             <tr>
                               <td>Address</td>
                               <td>
-                                South Fabien Street <br>
-                                No. 34
+                                {{(isset($orders->shipping)?$orders->shipping:'')}}
                               </td>
                             </tr>
-                            <tr>
-                              <td>County</td>
-                              <td>Bucharest</td>
-                            </tr>
-                            <tr>
-                              <td>City</td>
-                              <td>Bucharest</td>
-                            </tr>
-                            <tr>
-                              <td>Postal Code</td>
-                              <td>123456</td>
-                            </tr>
-                            <tr>
-                              <td>Phone</td>
-                              <td>+413-26-9811311</td>
-                            </tr>
-                            <tr>
-                              <td>Mobile phone</td>
-                              <td>+257-35-5785588</td>
-                            </tr>
-                            <tr>
-                              <td>Comment</td>
-                              <td>Lorem ipsum dolor sit amet.</td>
-                            </tr>
-                            </tbody>
-                          </table>
-                      </div>
-                      <div class="tab-pane" id="tab-billing-info">
-                        <h5>Billing company details</h5>
-                          <table class="table table-condensed table-hover">
-                            <tbody>
-                              <tr>
-                                <td>Company name</td>
-                                <td>Company Name</td>
-                              </tr>
-                              <tr>
-                                <td>Address</td>
-                                <td>
-                                  Flowers street <br>
-                                  No. 25
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>County</td>
-                                <td>Bucharest</td>
-                              </tr>
-                              <tr>
-                                <td>City</td>
-                                <td>Bucharest</td>
-                              </tr>
-                              <tr>
-                                <td>Tax Identification Number</td>
-                                <td>12345678</td>
-                              </tr>
-                              <tr>
-                                <td>Trade Registry Number</td>
-                                <td>J1/123/2000</td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        <h5>Billing address</h5>
-                          <table class="table table-condensed table-hover">
-                            <tbody>
-                              <tr>
-                                <td>Contact Person</td>
-                                <td>Jerry Williams</td>
-                              </tr>
-                              <tr>
-                                <td>Address</td>
-                                <td>
-                                  South Fabien Street <br>
-                                  No. 34
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>County</td>
-                                <td>Bucharest</td>
-                              </tr>
-                              <tr>
-                                <td>City</td>
-                                <td>Bucharest</td>
-                              </tr>
-                              <tr>
-                                <td>Postal Code</td>
-                                <td>123456</td>
-                              </tr>
-                              <tr>
-                                <td>Phone</td>
-                                <td>+413-26-9811311</td>
-                              </tr>
-                              <tr>
-                                <td>Mobile phone</td>
-                                <td>+257-35-5785588</td>
-                              </tr>
-                              <tr>
-                                <td>Comment</td>
-                                <td>Lorem ipsum dolor sit amet.</td>
-                              </tr>
                             </tbody>
                           </table>
                       </div>
@@ -218,18 +133,41 @@
           <!--/.row-->
 
           <div class="row">
-            <!--/.col-->
 
-            <div class="col-md-12">
+            <!-- <div class="col-md-12">
               <div class="card">
                 <div class="card-header">
-                  <span><i class="fa fa-truck"></i> Deliveries</span>
+                  <span><i class="fa fa-credit-card"></i> Payment Method</span>
                 </div>
                 <div class="card-body">
                   <table class="table table-condensed">
                     <thead>
                       <tr>
-                        <th>Type</th>
+                        <th>Type Payment</th>
+                        <th>Bank</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td class="vertical-align-middle">{{(isset($orders->payment[0]['type'])?$orders->member[0]['type']:'')}}</td>
+                        <td class="vertical-align-middle">{{(isset($orders->payment[0]['name'])?$orders->member[0]['name']:'')}}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div> -->
+            <!--/.col-->
+
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">
+                  <span><i class="fa fa-truck"></i> Data Delivery</span>
+                </div>
+                <div class="card-body">
+                  <table class="table table-condensed">
+                    <thead>
+                      <tr>
                         <th>Name</th>
                         <th>Price</th>
                         <th>Delivery Status</th>
@@ -238,11 +176,10 @@
                     </thead>
                     <tbody>
                       <tr>
-                        <td class="vertical-align-middle">Courier</td>
-                        <td class="vertical-align-middle">JNE</td>
-                        <td class="vertical-align-middle">200.000</td>
-                        <td class="vertical-align-middle">On Process (Warehouse rawamangun jakarta barat)</td>
-                        <td class="vertical-align-middle">2018/03/01</td>
+                        <td class="vertical-align-middle">{{(isset($orders->delivery[0]['name'])?$orders->delivery[0]['name']:'')}}</td>
+                        <td class="vertical-align-middle">{{(isset($orders->delivery[0]['price'])?$orders->delivery[0]['price']:'')}}</td>
+                        <td class="vertical-align-middle">{{(isset($orders->status)?$orders->status:'')}}</td>
+                        <td class="vertical-align-middle">{{(isset($orders->delivery[0]['created_at'])?$orders->delivery[0]['created_at']:'')}}</td>
                       </tr>
                     </tbody>
                   </table>
@@ -251,11 +188,62 @@
             </div>
             <!--/.col-->
 
-          </div>
-          <!--/.row-->
-        </div>
+            <div class="col-md-12">
+              <div class="card">
+                  <div class="card-header with-border">
+                        <span><i class="fa fa-shopping-cart"></i> Products Carts</span>
+                  </div>
+                  <div class="card-body">
+                    <div class="col-md-12">
+                      <table class="table table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>Product</th>
+                          <th>Price</th>
+                          <th>Quantity</th>
+                          <th class="text-right">Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($orders->products as $prod_list)
+                          <tr>
+                              <td class="vertical-align-middle">{{$prod_list['name']}}</td>
+                              <td class="vertical-align-middle">Rp. {{str_replace(',00','',number_format(isset($prod_list['price']),2,',','.'))}}</td>
+                              <td class="vertical-align-middle">{{$prod_list['quantity']}}</td>
+                              <td class="vertical-align-middle text-right">Rp. {{str_replace(',00','',number_format(isset($prod_list['totalPrice']),2,',','.'))}}</td>
+                          </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="col-md-12">
+                      <table class="table table-condensed">
+                          <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td class="text-right">
+                                    <strong>Total:</strong>
+                                </td>
+                                <td class="text-right">
+                                    <strong>{{(isset($orders->total_price)?'Rp. '.str_replace(',00','',number_format($orders->total_price,2,',','.')):'')}}</strong>
+                                </td>
+                            </tr>
+                          </tbody>
+                      </table>
+                  </div>
+                  </div>
+                </div>
+            </div>
+            <!--/.col-->
+        
+        <!--/.row-->
 
-      </div>
+    </div>
+    <!--/.row-->
+  </div>
+
+</div>
 @endsection
 
 @section('myscript')
